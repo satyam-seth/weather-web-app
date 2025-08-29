f9843e2b4198af0235899f3b36088fc585bf9813
Satyam Seth <satyam1998.1998@gmail.com>
2025-08-30 03:44:47 +0530

    Updates

--- src/api.ts
+++
@@ -1,9 +0,0 @@
-import { ClimateApi, Configuration } from "./client";
-
-const API_BASE_URL = import.meta.env.VITE_API_URL;
-
-const config = new Configuration({
-  basePath: API_BASE_URL,
-});
-
-export const climateApi = new ClimateApi(config);
