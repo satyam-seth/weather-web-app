1f551747630d6f04f667cadeef26a14aa9f7ac57
Satyam Seth <satyam1998.1998@gmail.com>
2025-08-30 03:30:18 +0530

    Read api base url from env file

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,9 @@
 import { ClimateApi, Configuration } from "./client";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const config = new Configuration({
-    basePath: API_BASE_URL
+  basePath: API_BASE_URL,
 });
 
 export const climateApi = new ClimateApi(config);
